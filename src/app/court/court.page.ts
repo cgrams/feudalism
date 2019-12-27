e146465e314200aa5aa8d2107791bcc12f74e9e6
Caleb Grams <calebgrams@192-168-1-105.tpgi.com.au>
2019-12-27 15:43:43 +1100

    first

---
+++ src/app/court/court.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-court',
+  templateUrl: 'court.page.html',
+  styleUrls: ['court.page.scss']
+})
+export class CourtPage {
+  constructor() {}
+
+    firstAnswerCourt:string = "";
+    secondAnswerCourtHidden:string= "";
+    firstAnswerCourtHidden:string = "";
+        onKey1(event) {
+            const inputValue = event.target.value;
+            console.log(inputValue);
+            if(inputValue == "me" || inputValue == "Me" || inputValue == "ME"){
+                this.firstAnswerCourt = "The next number is 3!";
+                 
+            }else{this.firstAnswerCourt = "Keep trying";}
+    }
+
+    onKey2(event){
+        const inputValue2 = event.target.value;
+        if(inputValue2 == "sorry"){
+            console.log('sorry works');
+            this.secondAnswerCourtHidden = "And then it's 5!";
+        }else{this.secondAnswerCourtHidden = "Keep trying";}
+    }
+}
